0821ae2406b8df65839379e8c27f8598bf718d49
Ali Mashreghi <22927673+amashreghi@users.noreply.github.com>
2021-09-17 14:12:47 -0700

    add multilayer perceptron classifier

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
 training_set = pd.read_csv('digit-recognizer-data/train.csv')
-x_test = pd.read_csv('digit-recognizer-data/test.csv')
-x_train = training_set.iloc[:,training_set.columns != 'label'].values
-y_train = training_set.iloc[:,training_set.columns == 'label'].values.ravel()
+test = pd.read_csv('digit-recognizer-data/test.csv')
+X = training_set.iloc[:,training_set.columns != 'label'].values
+y = training_set.iloc[:,training_set.columns == 'label'].values.ravel()
 
-print(x_test)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(16, 16), random_state=1, max_iter=10000)
+clf.fit(X, y)
+pred = clf.predict(test)
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-#clf.fit(X, y)
+output = pd.DataFrame({'ImageId': [(x+1) for x in range(len(pred))], 'Label': pred})
+
+output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
