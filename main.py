30b1bf698f95bbe218481462acf2a20078399dee
Ali Mashreghi <22927673+amashreghi@users.noreply.github.com>
2022-01-28 15:56:49 -0800

    improved accuracy to 93% using k-nearest algorithm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import math
 import pandas as pd
 from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 
 training_set = pd.read_csv('digit-recognizer-data/train.csv')
@@ -9,10 +10,17 @@ test = pd.read_csv('digit-recognizer-data/test.csv')
 X = training_set.iloc[:,training_set.columns != 'label'].values
 y = training_set.iloc[:,training_set.columns == 'label'].values.ravel()
 
-clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(16, 16), random_state=1, activation='logistic', learning_rate_init=0.1)
+print("Data processed!")
 
+#clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(8, 8), random_state=1, activation='logistic', learning_rate_init=0.2)
 
-scores = model_selection.cross_val_score(clf, X, y, scoring="accuracy", cv=10)
+# A simple k-nearest classifer works significantly better than a Multi-layer perteptron for this problem, i.e. 93% accuracy
+# Intuitively if two numbers are the same it's very likely that their pixel representation is also close
+clf = KNeighborsClassifier(n_neighbors=80)
+
+print("Classifier created!")
+
+scores = model_selection.cross_val_score(clf, X, y, scoring="accuracy", cv=5)
 print(scores)
 print(scores.mean())
 
